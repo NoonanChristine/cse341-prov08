e99d2645dc2b6143a1d66c2fa27cdaba16fe525f
Christine Noonan <noo19002@byui.edu>
2021-06-09 09:40:12 -0400

    finished prove08

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,24 +12,27 @@ let settings = {
 fetch(url, settings)
   .then(res => res.json())
   .then((json) => {
-    console.log(json)
+    global.products = json;
+    // console.log(json)
   });
 
-const renderIndex = (req, res, json) => {
+const renderIndex = async (req, res, json) => {
+  console.log('we get here');
   let searchedValue = req.body.searchValue || req.query.searchValue || '' // Handle for GET, POST or neither
   let page = req.query.page || 1 // Grab our page number, 1 if undefined
 
   const indexStart = (page - 1) * ITEMS_PER_PAGE // Item index to start on...
   const indexEnd = page * ITEMS_PER_PAGE
+  // const jsonResponse = req.url;
 
-  const filteredData = global.jsonResponse.filter(x =>
+  const filteredData = global.products.filter(x =>
     x.name.toLowerCase().includes(searchedValue.toLowerCase())
   )
 
   let stuff = {
     data: filteredData.slice(indexStart, indexEnd), // For JSON/Array and not Mongoose, .slice() works best.
     path: 'shop/product-list',
-    title: 'Prove 08',
+    pageTitle: 'Prove 08',
     searchedValue: searchedValue,
     page: page,
     numPages: Math.ceil(filteredData.length / ITEMS_PER_PAGE)
@@ -41,5 +44,11 @@ const renderIndex = (req, res, json) => {
 
 // New code for W08...
 exports.getIndex = (req, res, next) => {
+  // console.log('we get to this one');
+  renderIndex(req, res, global.jsonResponse) // Render page.
+}
+
+exports.processJson = (req, res, next) => {
+  // console.log('we get here too');
   renderIndex(req, res, global.jsonResponse) // Render page.
 }
